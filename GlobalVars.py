52031c3afe23e301cfc91568a522146312f10d5d
Gi1dor <89097892927@mail.ru>
2017-06-23 16:35:41 +0200

    Added ver. 0.9 ALPHA files. First upload

---
+++ GlobalVars.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+
+global logpath
+global path
+global patchpath
+global VERSION
+global cfg
+
+global app
+global wnd
+global progress_wnd
+global server_conn
+
+
---
+++ cenchanced.pp
@@ -0,0 +1,2 @@
+firstlaunch=1
+datpath=C:\Program Files (x86)\StandingStoneGames\The Lord of the Rings Online\client_local_English.dat
