27bac769079fce1c7562fca8a9ba1f52c2ed0edc
Gi1dor <89097892927@mail.ru>
2017-08-30 22:04:18 +0200

    Deleted HEALER.py due to implementing it's function into main program
    Deleted HEALER.py due to implementing it's function into main program. Deleted some rubbish

--- Pack.bat
+++ Pack.bat
@@ -1,6 +1,4 @@
 echo Сборка файлов. Требует python 2.7, WxPython, Pyinstaller
 echo Сборка Enchanced.exe
 pyinstaller --onefile main.spec
-echo Сборка healer.exe
-pyinstaller --onefile HEALER.spec
 pause
